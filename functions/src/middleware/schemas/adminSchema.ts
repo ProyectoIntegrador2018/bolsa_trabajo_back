ad1559e26371d99aecc37475d89637274cab1ce3
Aaron Garcia <20996397+AaronGarcia97@users.noreply.github.com>
2021-03-26 19:41:26 -0600

    Add schemas validation for Admin CRUD.

---
+++ functions/src/middleware/schemas/adminSchema.ts
@@ -0,0 +1,39 @@
+import * as Joi from 'joi';
+import { validateRequest } from '../../helpers/utility';
+
+export const adminSchema = {
+  create,
+  read,
+  update,
+  deletee,
+};
+
+export function create(req: any, res: any, next: any) {
+    const schema = Joi.object({
+        username: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required(),
+        phoneNumber: Joi.string().trim().regex(/^\+[0-9]{10}$/).required(),
+    });
+    validateRequest(req, res, next, schema);
+}
+
+export function read(req: any, res: any, next: any) {
+    const schema = Joi.object({});
+    validateRequest(req, res, next, schema);
+}
+
+export function update(req: any, res: any, next: any) {
+    const schema = Joi.object({
+        username: Joi.string().required(),
+        email: Joi.string().email().required(),
+        phoneNumber: Joi.string().trim().regex(/^\+[0-9]{10}$/).required(),
+    });
+    validateRequest(req, res, next, schema);
+}
+
+export function deletee(req: any, res: any, next: any) {
+    const schema = Joi.object({});
+    validateRequest(req, res, next, schema);
+}
+
